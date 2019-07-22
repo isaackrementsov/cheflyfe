7e95e583446a0b4f7e41aac2bb77fa9162d35fd0
Isaac Krementsov <isaackrementsov@gmail.com>
2019-07-22 14:02:44 -0400

    Auth middleware added

--- src/util/Middleware.ts
+++ src/util/Middleware.ts
@@ -3,7 +3,16 @@ import {Request, Response, NextFunction} from 'express';
 export default class Middleware {
 
     auth = (req: Request, res: Response, next: NextFunction) => {
-        next();
+        let adminRestricted : boolean = req.url.indexOf('/admin') != -1;
+        let userRestricted : boolean = !(req.url == '/' || req.url == '/login' || req.url == '/signup');
+
+        if(adminRestricted && req.session.admin || userRestricted && req.session.username || !adminRestricted && !req.session.admin || !userRestricted && !req.session.username){
+            next();
+        }else if(adminRestricted && !req.session.admin || userRestricted && !req.session.username){
+            res.redirect('/login');
+        }else if(!userRestricted && req.session.user){
+            res.redirect('/users/' + req.session.username);
+        }
     }
 
 }
